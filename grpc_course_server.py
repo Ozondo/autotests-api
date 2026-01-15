6379ce13e7ea9a89b309314e776a0e234b911a20
Egor Ivanov <101597786+Ozondo@users.noreply.github.com>
2026-01-15 15:54:49 +0300

    HW 5.5

---
+++ grpc_course_server.py
@@ -0,0 +1,26 @@
+import grpc
+from concurrent import futures
+
+import course_service_pb2_grpc
+import course_service_pb2
+
+
+class CourseServiceServicer(course_service_pb2_grpc.CourseServiceServicer):
+    def GetCourse(self, request, context):
+        print(f"На вход получили id = {request.course_id}")
+        return course_service_pb2.GetCourseResponse(
+            course_id=request.course_id,
+            title="Автотесты API",
+            description="Будем изучать написание API автотестов"
+        )
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    course_service_pb2_grpc.add_CourseServiceServicer_to_server(CourseServiceServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("gRPC сервер запущен")
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
