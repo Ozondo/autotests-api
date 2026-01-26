c4de1c58d5e71b644d0fde00efe3fb0c37654ca3
Egor Ivanov <101597786+Ozondo@users.noreply.github.com>
2026-01-27 00:44:15 +0300

    HomeWork 5.6

---
+++ websocket_users_client.py
@@ -0,0 +1,19 @@
+import asyncio
+
+import websockets
+
+
+async def client():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        message = "Привет, Сервер"
+        await websocket.send(message)
+
+        for i in range(5):
+            response = await websocket.recv()
+            print(f"{i+1} {response}")
+
+
+
+
+asyncio.run(client())
