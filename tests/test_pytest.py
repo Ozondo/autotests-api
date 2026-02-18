677c2ba1e81ed2a7bce65940dfec8d847df49e94
Egor Ivanov <101597786+Ozondo@users.noreply.github.com>
2026-02-18 17:45:47 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,11 @@
+def test_first_try():
+    print("test_first_try")
+
+def test_second_try():
+    pass
+
+
+def test_assert_positive():
+    assert 1 + 1 == 2
+def test_assert_negative():
+    assert -1 == 1
