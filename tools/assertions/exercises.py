76a520061936a998aa4b1a43aa558ccab0dcbeb9
Egor Ivanov <101597786+Ozondo@users.noreply.github.com>
2026-03-05 14:52:44 +0300

    H.W. 9.4 3 задание

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,5 +1,5 @@
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, ExercisesResponseSchema, ExerciseSchema, \
-    GetExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema
 from tools.assertions.base import assert_equal
 
 
@@ -48,3 +48,18 @@ def assert_get_exercise_response(
     :raises AssertionError: Если данные файла не совпадают.
     """
     assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
+
+def assert_update_exercise_response(request: UpdateExerciseRequestSchema, response: ExercisesResponseSchema):
+    """
+    Проверяет, что данные на обновления упражнения соответствуют обновленным данным упражнения.
+
+    :param request: Исходный запрос на обновление упражнения.
+    :param response: Ответ API с данными упражнения.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(request.title, response.exercise.title, "title")
+    assert_equal(request.max_score, response.exercise.max_score, "max_score")
+    assert_equal(request.min_score, response.exercise.min_score, "min_score")
+    assert_equal(request.order_index, response.exercise.order_index, "order_index")
+    assert_equal(request.description, response.exercise.description, "description")
+    assert_equal(request.estimated_time, response.exercise.estimated_time, "estimated_time")
